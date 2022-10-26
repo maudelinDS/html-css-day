f26098a5ef7f61351680a89508cc0088e15e8af2
maudelinDS <maudelin.delescaut@gmail.com>
2022-10-26 16:15:38 +0200

    function average for grades

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 console.log("Bonjour");
 
 // (object)permet dacceder au dom
@@ -18,7 +21,7 @@ toto.addEventListener("click", function () {
 let pp = document.getElementById("ii")
 // (innerText) variable représente un élément dans le html
 
-pp.addEventListener("input", function () {
+pp.addEventListener("input", function (e) {
 
     console.log("j'ai cliqué");
 
@@ -27,7 +30,58 @@ pp.addEventListener("input", function () {
   for (const article of articles) {
     article.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
   }
+
+ console.log(e.target.value)
+
+document.getElementsByTagName("h1")[0].innerText = e.target.value
+
 });
 
+
+//Grades average computing
+
+// Display average
+
+function displayAverage(avg, id) {
+document.getElementById(id).innerText = avg
+}
+
+// Compute average
+function average (gradesArray){
+    let sum = 0
+    for (const grade of gradesArray) {
+
+        sum += grade
+    }
+    return sum / gradesArray.length
+}
+
+//Retrieve grades
+
+function retrieveGrades(gradesID) {
+
+    let grades = []
+
+    for (const gradesIDElement of gradesID) {
+
+grades.push (parseInt(document.getElementById(gradesIDElement).value))
+    }
+    return grades
+}
+
+//Register listener
+let inputs = document.getElementsByTagName('input')
+
+for (const input of inputs) {
+    input.addEventListener('input',function (){
+
+        displayAverage(average(retrieveGrades(['grade-1', 'grade-2'])),
+
+            'average'
+
+        )
+
+    })
+}
 
 
